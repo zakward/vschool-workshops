2f015723b147bcf67bf7357c3ec7d693492da417
zakward85 <zakward85@gmail.com>
2023-08-28 22:26:03 -0500

    first push

--- lvl1workshop2/main.js
+++ lvl1workshop2/main.js
@@ -4,18 +4,7 @@ const playerName = readline.question("Hello!  What is your name?"); // readline 
 
 console.log(`Welcome ${playerName} to the RPG game!!! `); // consoles a message using the users playerName
 
-//ES5  Function Expression
-// function Character(name, healthPoints, attackPoints, inventory) {
-//   this.name = name;
-//   this.healthPoints = healthPoints;
-//   this.attackPoints = attackPoints;
-//   this.inventory = inventory;
-//   this.printInventory = function () {
-//     console.log(`Name: ${this.name}`);
-//     console.log(`Inventory: ${this.inventory}`);
-//   };
-// }
-
+let isGameRunning = true;
 //ES6 - Class constructor
 class Character {
   constructor(name, healthPoints, attackPoints, inventory) {
@@ -37,26 +26,48 @@ const enemy1 = new Character("Tricky Tim", 25, 2, ["Tooth", "Bracelet"]);
 const enemy2 = new Character("Lulu", 36, 3, ["Sword", "Necklace"]);
 const enemy3 = new Character("Mickey", 18, 1, ["Ears"]);
 
-// console.log(hero);
-// console.log(enemy1);
-// console.log(enemy2);
-// console.log(enemy3);
-
 let enemies = [enemy1, enemy2, enemy3]; // creates an array of all instantiated enemies
-
-// console.log(enemies);
-
-hero.printInventory(); // prints the inventory of the hero
-
-// enemy1.printInventory();
 
 const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-// console.log(getRandomNumber(1, 10));
+while (isGameRunning) {
+  let action = readline.question(
+    "What do you want to do? Press [w] to walk, [i] to see your inventory, or [q] to quit",
+    { limit: ["w", "i", "q"] }
+  );
+  // console.log(action);
+  if (action === "w") {
+    console.log("You Chose to Walk");
 
-// hero.printInventory();
+    let enemyChance = getRandomNumber(1, 4);
+    console.log(enemyChance);
+
+    if (enemyChance === 2) {
+      let randomEnemyIndex = getRandomNumber(0, enemies.length - 1);
+      let currEnemy = enemies[randomEnemyIndex];
+      console.log("An enemy has appeared");
+      console.log(currEnemy);
+      const options = ["fight", "run"];
+      let fightOrRun = readline.keyInSelect(
+        options,
+        "do you want to fight or run?"
+      );
+      console.log(options[fightOrRun]);
+      if (options[fightOrRun] === "fight") {
+        fight();
+      } else if (options[fightOrRun] === "run") {
+        run();
+      }
+    }
+  } else if (action === "i") {
+    hero.printInventory();
+  } else if (action === "q") {
+    console.log("You chose to quit!");
+    isGameRunning = false;
+  }
+}
 
 function fight() {
   console.log("You Chose to FIGHT");
@@ -66,12 +77,11 @@ function run() {
   console.log("You Chose to RUN");
 }
 
-const options = ["fight", "run"];
-let fightOrRun = readline.keyInSelect(options, "Do you want to fight or run??");
+// const options = ["fight", "run"];
+// let fightOrRun = readline.keyInSelect(options, "do you want to fight or run?");
 
-if (options[fightOrRun] === "fight") {
-  fight();
-} else if (options[fightOrRun] === "run") {
-  run();
-}
-
+// if (options[fightOrRun] === "fight") {
+//   fight();
+// } else if (options[fightOrRun] === "run") {
+//   run();
+// }
